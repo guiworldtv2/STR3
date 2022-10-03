4d9f24c835aff3f56f3d8e9e7aba264b8b83df78
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2022-10-02 23:45:07 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -72,7 +72,7 @@ def generate_youtube_tv():
                         # Just a video
                         video = result
                 video_url = video['url']
-                canalnome = video['id']
+                canalnome = video['resolution']
                 
 
                 channel_no += 1
