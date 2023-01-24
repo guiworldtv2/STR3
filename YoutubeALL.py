03fa7c8e19cc892bb8854096f52de9a389186d78
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-01-24 15:33:26 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -31,17 +31,6 @@ def get_live_info(channel_id):
     except Exception as e:
                 return None
 
-banner = r'''
-
-#EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"
-#EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"
-
-'''
-
-
-
 def generate_youtube_tv():
     global channel_no
     ydl_opts = {
