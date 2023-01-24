e2be94405e0b6a3894d81892b1306f47be3bdaa9
guiworldtv <95205982+guiworldtv@users.noreply.github.com>
2023-01-23 21:19:15 -0300

    Update YoutubeALL.py

--- YoutubeALL.py
+++ YoutubeALL.py
@@ -1,12 +1,14 @@
 #! /usr/bin/python3
-
 from __future__ import unicode_literals
 import youtube_dl
 import requests
 import shutil
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import streamlink
+
 channel_no = 1
+m3u = None
 
 def get_live_info(channel_id):
     try:
@@ -27,18 +29,16 @@ def get_live_info(channel_id):
             "image": imageMeta.get("content"),
             "description": descriptionMeta.get("content")
         }
-    
     except Exception as e:
                 return None
 
 
-banner = r'''
 
+banner = r'''
 #EXTM3U x-tvg-url="https://iptv-org.github.io/epg/guides/ar/mi.tv.epg.xml"
 #EXTM3U x-tvg-url="https://raw.githubusercontent.com/mudstein/XML/main/TIZENsiptv.xml"
 #EXTM3U x-tvg-url="https://raw.githubusercontent.com/K-vanc/Tempest-EPG-Generator/main/Siteconfigs/Argentina/%5BENC%5D%5BEX%5Delcuatro.com_0.channel.xml"
 #EXTM3U x-tvg-url="https://raw.githubusercontent.com/Nicolas0919/Guia-EPG/master/GuiaEPG.xml"
-
 '''
 
 def generate_youtube_tv():
@@ -48,32 +48,43 @@ def generate_youtube_tv():
     }
     ydl = youtube_dl.YoutubeDL(ydl_opts)
 
-    with open('YoutubeALL.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip()
-            if line == "":
-                continue
-            channel = get_live_info(line)
-            if channel is None:
-                continue
+    with open('YoutubeALL.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if line == "":
+            continue
+        channel = get_live_info(line)
+        if channel is None:
+            continue
+        try:
+            with ydl:
+                result = ydl.extract_info(
+                    f"{line}/live",
+                    download=False  # We just want to extract the info
+                )
+
+                if 'entries' in result:
+                    # Can be a playlist or a list of videos
+                    video = result['entries'][-1]
+                else:
+                    # Just a video
+                    video = result
+            video_url = video['url']
+            canalnome = video['uploader']
+            viewrs = video['view_count']
+
+            channel_no += 1
+            channel_name = f"{channel_no}-{line.split('/')[-1]}"
+            playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{canalnome}\" tvg-base=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"YOUTUBE\",{canalnome} - {channel.get('title')} - {viewrs}\n"
+            write_to_playlist(playlistInfo)
+            write_to_playlist(video_url)
+            write_to_playlist("\n")
+            
+        except Exception as e:
             try:
-                with ydl:
-                    result = ydl.extract_info(
-                        f"{line}/live",
-                        download=False  # We just want to extract the info
-                    )
-
-                    if 'entries' in result:
-                        # Can be a playlist or a list of videos
-                        video = result['entries'][-1]
-                    else:
-                        # Just a video
-                        video = result
-                video_url = video['url']
-                canalnome = video['uploader']
-                viewrs = video['view_count']
-
+                streams = streamlink.streams(line)
+                video_url = streams["best"].url
                 channel_no += 1
                 channel_name = f"{channel_no}-{line.split('/')[-1]}"
                 playlistInfo = f"#EXTINF:-1 tvg-chno=\"{channel_no}\" tvg-id=\"{canalnome}\" tvg-base=\"{line}\" tvg-name=\"{channel_name}\" tvg-logo=\"{channel.get('image')}\" group-title=\"YOUTUBE\",{canalnome} - {channel.get('title')} - {viewrs}\n"
@@ -81,6 +92,8 @@ def generate_youtube_tv():
                 write_to_playlist(video_url)
                 write_to_playlist("\n")
             except Exception as e:
+               
+
                 print(e)
 
                         
